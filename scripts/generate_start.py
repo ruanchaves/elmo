f2ca5085d80cc3c8df0e7df1cfc81a0ebeae7fb9
ruanchaves <ruanchaves@gmail.com>
2020-02-11 08:20:07 -0300

    sudo screen

--- scripts/generate_start.py
+++ scripts/generate_start.py
@@ -7,7 +7,7 @@ cmd_strings = ["cd \"$(dirname \"${BASH_SOURCE[0]}\")\""]
 
 for root, dirs, files in os.walk("../settings", topdown=False):
    for name in files:
-       cmd = "screen -dmS {4} bash -c 'sudo TESTS={0} RESULTS={1} BERT_DIR={2} CUDA_VISIBLE_DEVICES={3} bash start.sh'"
+       cmd = "sudo screen -dmS {4} bash -c 'sudo TESTS={0} RESULTS={1} BERT_DIR={2} CUDA_VISIBLE_DEVICES={3} bash start.sh'"
        result = 'result_' + name.rstrip('yaml') + 'json'
        screen_name = name.rstrip('.yaml')
        cmd_strings.append(cmd.format(name, result, bert, cuda, screen_name))
